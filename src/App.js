9d72b4ef4e859525c1da2971dd294c6cacb01093
sandhya <79160128+sandhyabaiju86@users.noreply.github.com>
2023-10-24 11:00:19 +0400

    updated json and app.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <h1>Weather Search Engine</h1>
       <footer>
-        <a href=''
+        <p>This project is created by <a href='mailti:sandhyacc28@gmail.com'> Sandhya Baiju</a> and is
+        <a href='https://github.com/sandhyabaiju86/weather-search-engine' target='_blank' rel='noreferrer'> Open Source on GitHub</a>
+        </p>
       </footer>
     </div>
   );
