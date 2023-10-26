1f224da7eb4bddc82b27efefb68c0698f5e30e74
sandhya <79160128+sandhyabaiju86@users.noreply.github.com>
2023-10-26 14:53:15 +0400

    changed

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from 'axios';
+
 import "./Weather.css";
 
 export default function Weather(){
+    const[loaded,setLoaded]=useState(false);
+    const[weatherData, setWeatherData]=useState({})
+function handleResponse(response){
+    setLoaded(true);
+   setWeatherData({
+    temperature:Math.round(response.data.main.temp),
+    humidity:response.data.main.humidity,
+    wind:response.data.wind.speed,
+    city:response.data.name,
+    description:response.data.weather[0].escription,
+    iconUrl:"https://ssl.gstatic.com/onebox/weather/64/rain_s_cloudy.png",
+
+   })
+}
+
+if(loaded){
+
+    
     return(
         <div className="Weather"> 
         <form>
@@ -18,28 +38,35 @@ export default function Weather(){
             </div>
         </form>
         <div>
-        <h1>Dubai</h1>
+        <h1>{weatherData.city}</h1>
         <ul>
             <li>Wednesday 11AM</li>
-            <li>Mostly sunny</li>
+            <li>{weatherData.description}</li>
         </ul>
         </div>
         <div>
             <div className="row mt-4">
                 <div className="col-6">
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostly sunny"/>
-                    <span className="temperature">34</span>
+                    <img src={weatherData.iconUrl} alt={weatherData.description}/>
+                    <span className="temperature">{weatherData.temperature}</span>
                     <span className="unit">&deg;C</span>
                 </div>
                 <div className="col-6">
                     <ul>
-                        <li>Precipitation : 1%</li>
-                        <li>Humidity : 38%</li>
-                        <li>Wind : 18Km/h</li>
+                        <li>Precipitation :{weatherData.precipitation}</li>
+                        <li>Humidity : {weatherData.humidity}%</li>
+                        <li>Wind : {weatherData.wind}Km/h</li>
                     </ul>
                 </div>
             </div>
         </div>
         </div>
     )
+}
+else{
+    const apiKey = "ce144f0cf51fa43f03431f0488a36728"
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kerala&appid=${apiKey}`
+    axios.get(apiUrl).then(handleResponse)
+
+}
 }
