11b8c7fa202b2e26234b8447bad34d82c9c717ee
sandhya <79160128+sandhyabaiju86@users.noreply.github.com>
2023-10-31 11:06:34 +0400

    added animated weather icon.Made some changes to weather.css file

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon"  
 export default function WeatherInfo(props){
     return(
         <div>
@@ -13,8 +14,9 @@ export default function WeatherInfo(props){
         <div>
             <div className="row mt-4">
                 <div className="col-6">
-                    <img className="text-capitalize" src={props.data.iconUrl} alt={props.data.description}/>
-                    <span className="temperature">{props.data.temperature}</span>
+                    <WeatherIcon code={props.data.icon} className="float-left"/>
+                    {/* <img className="text-capitalize" src={props.data.iconUrl} alt={props.data.description}/> */}
+                    <span className="temperature float-left">{props.data.temperature}</span>
                     <span className="unit">&deg;C</span>
                 </div>
                 <div className="col-6">
